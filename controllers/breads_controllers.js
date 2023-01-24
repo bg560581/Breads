35f3653c0db55a850b4dcc3d9a994a418c64d0ea
bg560581 <bennett.galgano@gmail.com>
2023-01-23 21:05:34 -0500

    sending index and show data

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -7,14 +7,14 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('index',
-        {
-            breads: Bread,
-            title: 'Index Page'
-        }
+    res.render('Index',
+      {
+        breads: Bread,
+        title: 'Index Page'
+      }
     )
-  // res.send(Bread)
 })
+
 
 
 // SHOW
