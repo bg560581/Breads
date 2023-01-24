6a1a8452715e29c8119030e214ec759ebe64c832
bg560581 <bennett.galgano@gmail.com>
2023-01-23 20:52:40 -0500

    sending index and show data

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -5,13 +5,33 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 
+// INDEX
 breads.get('/', (req, res) => {
-    res.send(Bread)
+    res.render('index',
+        {
+            breads: Bread
+        }
+    )
+  // res.send(Bread)
 })
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     res.send(Bread[req.params.arrayIndex])
 })
+
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+    if (Bread[req.params.indexArray]){
+      Bread.splice(req.params.indexArray, 1)
+    res.status(303).redirect('/breads')
+    }  else {
+      res.send('Bread DNE')
+    }
+    
+  })
+  
 
 module.exports = breads
