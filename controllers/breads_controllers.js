7fde21e16c553b1d89db61cf93b80b431b46c0f6
bg560581 <bennett.galgano@gmail.com>
2023-01-24 21:04:52 -0500

    sending index and show data

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -23,9 +23,10 @@ breads.get('/:arrayIndex', (req, res) => {
         bread:Bread[req.params.arrayIndex]
       })
     } else {
-      res.send('404')
+      res.render('404')
     }
   })
+  
   
 
 // // DELETE
