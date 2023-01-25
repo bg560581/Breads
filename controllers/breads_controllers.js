30c9f4e04cd4b81b64a101c8b6f2544e59adf098
bg560581 <bennett.galgano@gmail.com>
2023-01-24 21:04:06 -0500

    sending index and show data

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -15,24 +15,29 @@ breads.get('/', (req, res) => {
     )
 })
 
-
-
+// SHOW
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-})
-
-
-// DELETE
-breads.delete('/:indexArray', (req, res) => {
-    if (Bread[req.params.indexArray]){
-      Bread.splice(req.params.indexArray, 1)
-    res.status(303).redirect('/breads')
-    }  else {
-      res.send('Bread DNE')
+    if (Bread[req.params.arrayIndex]) {
+      res.render('Show', {
+        bread:Bread[req.params.arrayIndex]
+      })
+    } else {
+      res.send('404')
     }
-    
   })
+  
+
+// // DELETE
+// breads.delete('/:indexArray', (req, res) => {
+//     if (Bread[req.params.indexArray]){
+//       Bread.splice(req.params.indexArray, 1)
+//     res.status(303).redirect('/breads')
+//     }  else {
+//       res.send('Bread DNE')
+//     }
+    
+//   })
   
 
 module.exports = breads
--- server.js
+++ server.js
@@ -10,11 +10,19 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
 
 // routes
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
 
 // Breads
 const breadsController = require('./controllers/breads_controllers.js')
