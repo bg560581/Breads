e13b6da48b46f275a3bf20eb6b1c2d6d2c6cfb81
bg560581 <bennett.galgano@gmail.com>
2023-01-29 18:17:31 -0500

    Adding Message

--- models/bread.js
+++ models/bread.js
@@ -10,6 +10,11 @@ const breadSchema = new Schema({
     image: { type: String, default: 'http://placehold.it/500x500.png' }
 })
 
+// helper methods 
+breadSchema.methods.getBakedBy = function(){
+  return `${this.name} was baked with love by ${this.baker}`
+}
+
 // model and export 
 const Bread = mongoose.model('Bread', breadSchema)
 module.exports = Bread
